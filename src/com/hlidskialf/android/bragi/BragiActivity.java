c5b76ba99957f7afd80421e94e47701dacb16ea1
code@hlidskialf.com <code@hlidskialf.com@c9831883-abd0-4b9c-fdc0-0e110619782c>
2010-06-22 05:12:35 +0000

    skeleton
    git-svn-id: https://hlidskialf-android-bragi.googlecode.com/svn/trunk@4 c9831883-abd0-4b9c-fdc0-0e110619782c

---
+++ src/com/hlidskialf/android/bragi/BragiActivity.java
@@ -0,0 +1,14 @@
+package com.hlidskialf.android.bragi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BragiActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
