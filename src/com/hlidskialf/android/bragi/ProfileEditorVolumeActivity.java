fa36a1208dd3f67a7c131434f317023b8672d315
code@hlidskialf.com <code@hlidskialf.com@c9831883-abd0-4b9c-fdc0-0e110619782c>
2010-07-10 00:47:14 +0000

    profile editor preferences
    git-svn-id: https://hlidskialf-android-bragi.googlecode.com/svn/trunk@20 c9831883-abd0-4b9c-fdc0-0e110619782c

--- src/com/hlidskialf/android/bragi/ProfileEditorVolumeActivity.java
+++ src/com/hlidskialf/android/bragi/ProfileEditorVolumeActivity.java
@@ -3,7 +3,6 @@ package com.hlidskialf.android.bragi;
 
 import android.preference.PreferenceActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,17 +14,18 @@ import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.RingtonePreference;
 
+/* THIS IS REALLY DIFFERENT I SWEAR */
 
-public class ProfileEditorActivity extends PreferenceActivity
+
+public class ProfileEditorVolumeActivity extends PreferenceActivity
                 implements View.OnClickListener
 {
-    public static final int RESULT_VOLUME_SCREEN=1;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        addPreferencesFromResource(R.xml.profile_editor);
+        addPreferencesFromResource(R.xml.profile_editor_volume);
 
         ImageView v;
         v = (ImageView)findViewById(R.id.actionbar_logo);
@@ -36,23 +36,8 @@ public class ProfileEditorActivity extends PreferenceActivity
         v.setOnClickListener(this);
         v.setImageResource(android.R.drawable.ic_menu_close_clear_cancel);
         v.setVisibility(View.VISIBLE);
-
-        BragiDatabase mDbHelper = new BragiDatabase(this);
-        Cursor mSlotCursor = mDbHelper.getAllSlots();
-
-        PreferenceScreen screen = getPreferenceScreen();
-
-        int l = mSlotCursor.getCount();
-        final int idx = mSlotCursor.getColumnIndex(BragiDatabase.SlotColumns.NAME);
-        for(; mSlotCursor.moveToNext(); ) {
-            final String name = mSlotCursor.getString(idx);
-            RingtonePreference pref = new RingtonePreference(this);
-            pref.setTitle(name);
-            pref.setSummary("ringtone for the '"+name+"' slot");
-            screen.addPreference(pref);
-        }
     }
-
+/* THIS IS REALLY DIFFERENT I SWEAR */
     public void onClick(View v) 
     {
         final long id = v.getId();
@@ -60,14 +45,5 @@ public class ProfileEditorActivity extends PreferenceActivity
           finish();
         }
     }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference)
-    {
-        final String pref_key = preference.getKey();
-        if (pref_key.equals("volume_screen")) {
-            Intent intent = new Intent(this, ProfileEditorVolumeActivity.class);
-            startActivityForResult(intent, RESULT_VOLUME_SCREEN);
-        }
-        return true;
-    }
+/* THIS IS REALLY DIFFERENT I SWEAR */
 }
